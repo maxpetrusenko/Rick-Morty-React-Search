9a663510bc7195b0bedc53628c2de7861c577b9c
max petrusenko <max_p@maxpetrusenko-2.local>
2020-06-14 14:56:04 -0700

    1st

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React,{ useState, useEffect } from "react";
+import './App.css';
+import Character from './components/Character';
+import axios from "axios"
+import SearchBar from "./components/SearchBar";
+
+
+const App = () => {
+  const [cards,setQuery] = useState([]);
+
+  useEffect(()=>{
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then((response)  =>{
+        console.log(response.data.results)
+        setQuery(response.data.results);
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+},[])
+
+  return (
+    <div className="App">
+      <h1 className="Header">Characters</h1>
+      <div className="cards">
+        <SearchBar 
+          characters={cards}
+        ></SearchBar>
+      </div>
+    </div>
+  );
+}
+
+export default App;
